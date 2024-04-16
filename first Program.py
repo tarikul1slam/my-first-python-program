ee5ba39308a938e72f878bdfe0a0ce5a5077aa8f
Tareque Islam <104964758+itarikislam@users.noreply.github.com>
2024-04-16 12:26:16 +0600

    Add files via upload

---
+++ first Program.py
@@ -0,0 +1,143 @@
+print("Hello World")
+a = 2
+print(a)
+print("My name is Tarek")
+print("My age is 21")
+print("I'm a University 2nd year student.")
+print("My name is Tarikul Islam,""I'm 21 years old.""I'm University 2nd year student.")
+#Numbers Print
+b = 8
+print(a+b)
+print(8+2)
+name = "Tarikul Islam"
+age = 21
+price = 99.99
+x = "2"
+print("Name:", name)
+print("Age:",age)
+
+print("My name is", name, "My age", age)
+print("I'm", name, "and my age is", age)
+
+#Variables type find in python.
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(x))
+
+#Boolean in python
+age1 = 21
+old = False
+a2 = None
+print(type(age1))
+print(type(old))
+print(type(a2))
+
+#Sum in python
+a1 = 2
+b1 = 5
+sum = a1 + b1
+print(sum)
+print(a1**b1)
+print(a1/b1)
+print(a1-b1)
+print(a1*b1)
+
+#Relational Operator
+print(a1 == b1)
+print(a1 != b1)
+print(a1 >= b1)
+print(a1 <= b1)
+print(a1 < b1)
+
+#Assignment Operators
+num = 10
+num = num + 10
+print(num)
+num1 = 10
+num1 += 10
+print(num1)
+num2 = 12
+num2 -=12
+print(num2)
+num3 = 12
+num3 /= 4
+print(num3)
+num4 = 12
+num4 *= 4
+print("num:", num4)
+print(num)
+num5 = 48
+num5 += 12
+print("num5:", num5 )
+
+#Logical Operators
+a = 5
+b = 2
+print(not a < b)
+print( a != b)
+print(not False)
+print(not True)
+
+#And OR Operators
+a = True
+b = True
+print("And Operators:", a and b)
+a1 = True
+b1 = False
+print("And Operators:", a1 and b1)
+print("Or Operators:", a1 or b1)
+print("Or Operators:", a or b)
+d = 5
+f= 6
+print("And Operators:", d == f and d < f)
+print("Or Opetators:", d < f or d < f)
+
+#Type Convertion
+a = int("5")
+b = 8
+sum = a + b
+print(sum)
+a1 = float("4")
+b1 = 23.5
+sum = a1 + b1
+print(sum)
+
+#Input in Python
+name = input("Your Name: ")
+print("Welcome ", name)
+age = float(input("Your Age: "))
+print("Age ", age)
+print(type(age), age)
+
+name1 = input("Your Name: ")
+age1 = input("Age: ")
+mark = input("Marks: ")
+print("Welcome ", name1)
+print("Your Age:", age1)
+print("Your Marks:", mark)
+
+#Input 2 numbers and sum the number
+first = int(input("First Number="))
+second = int(input("Second Numbr="))
+sum = first + second
+print(sum)
+#input 2 numbers and sum
+a = int(input("First Number: "))
+b = int(input("Second Number:"))
+c = int(input("Third Number: "))
+sum = a + b + c
+print(sum)
+
+#WAP = Write A Program
+#WAP to input a Square side and find area
+a8 = int(input("Write Square Lenth: "))
+b8 = int(input("Write Square Wide: "))
+sum8 = a*b
+print(sum8)
+
+#WAP floating num and find their average
+a9 = int(input("First Num: "))
+b9 = int(input("Second Num"))
+sum = (a + b)/2
+print(sum)
